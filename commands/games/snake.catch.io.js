113185824d05fd0694fce75bb6ea8cd5f8ca851f
satya1403 <83737281+satya1403@users.noreply.github.com>
2023-10-03 10:43:43 -0700

    Update snake.catch.io.js

--- commands/games/snake.catch.io.js
+++ commands/games/snake.catch.io.js
@@ -2,7 +2,7 @@ import('discord.js'); require('discord.js'); const disbut = require('discord.js-
 import('discord.js-buttons'); 
 const Discord = require('discord.js'); 
 
-const token = 'MTE1NTY2MzYyNDAxOTM5MDQ3NAGo3dUk.l56yj1nHd9ys2IJrQOVay_ksLqB5Rzbvs7vHIw'
+const token = token'
 client.login(token)
 
 const Discord = require('discord.js');
