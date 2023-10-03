0aa507c9225f1c9861c7567d8b9b3bbc5f31f575
satya1403 <83737281+satya1403@users.noreply.github.com>
2023-10-03 10:43:21 -0700

    Update tictactoe.js

--- commands/games/tictactoe.js
+++ commands/games/tictactoe.js
@@ -2,5 +2,5 @@ import('discord.js'); require('discord.js'); const disbut = require('discord.js-
 import('discord.js-buttons'); 
 const Discord = require('discord.js');
 
-const token = 'MTE1NTY2MzYyNDAxOTM5MDQ3NAGo3dUk.l56yj1nHd9ys2IJrQOVay_ksLqB5Rzbvs7vHIw'
+const token = 'token'
 client.login(token)
